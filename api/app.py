929c7aa23f7e335712c9bca09b396110b0b6605d
Shreeti Shrestha <shreetis13@gmail.com>
2025-07-28 20:57:18 +0545

    cors error solved

--- api/app.py
+++ api/app.py
@@ -27,7 +27,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "varysnp.netlify.app"],
+    allow_origins=["http://localhost:5173", "https://varysnp.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

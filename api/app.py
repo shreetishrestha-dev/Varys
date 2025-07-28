7d1feb2e973d06f49a6369c7047b8f8fd33f3447
Shreeti Shrestha <shreetis13@gmail.com>
2025-07-28 20:44:39 +0545

    requm and deployment

--- api/app.py
+++ api/app.py
@@ -27,7 +27,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "varysnp.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- varys-frontend/src/api/appApi.js
+++ varys-frontend/src/api/appApi.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://localhost:8000";
-
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 // Fetch companies from the backend
 export const fetchCompanies = async () => {
   try {
